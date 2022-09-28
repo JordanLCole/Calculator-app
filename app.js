18c287ca37791c1ccf79471858c7e85cc9c10c01
Jordan <jordancole2245@gmail.com>
2022-09-28 12:48:00 +0100

    Grid added, js started

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 "use strict";
+
+//button event listeners
+
+let one = document.querySelector("#1");
+
+//add to window
+
+one.addEventListener("click", function() {
+    document.querySelector(".display-window").textContent = '1';
+});
+
