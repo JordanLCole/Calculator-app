2ea72e1d531113cc5fee63860f70f25ad51a52d8
Jordan <jordancole2245@gmail.com>
2022-09-29 15:52:54 +0100

    Functional calculator - needs decimalisation add

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
 "use strict";
 
 //display window
-let displayWindow = document.querySelector('.display-window');
+let displayWindow = document.querySelector('#display-window');
+let calculatingWindow = document.querySelector('#calculating');
 let displayValue = '';
-let firstValue = null;
 let currentOperator = '';
+let currentTotal = '';
+let answer = ''
 
 //Operators
 const clear = document.querySelector('#clear');
+const clearAll = document.querySelector('#clear-all');
 const divide = document.querySelector('#divide');
 const calculateEquals = document.querySelector('#equals');
 const multiply = document.querySelector('#multiply');
+const subtract = document.querySelector('#minus');
+const add = document.querySelector('#plus');
 
 //numbers
 const buttons = document.querySelectorAll('button[id].value')
+const decimal = document.querySelector('#decimal');
 
 //key press event listeners
 clear.addEventListener('click', clearDisplayWindow);
+clearAll.addEventListener('click', clearMemory);
 divide.addEventListener('click', operateDivide);
 calculateEquals.addEventListener('click', calculate);
 multiply.addEventListener('click', operateMultiply);
+decimal.addEventListener('click', decimalAdded);
+subtract.addEventListener('click', operateSubtract);
+add.addEventListener('click', operateAddition);
+
 
 //functions
 
 //clears the display window and sets current value to 0
 function clearDisplayWindow(event) {
     console.log('display window cleared');
-    displayWindow.textContent = '0';
-    currentOperator = ''
+    displayWindow.textContent = '';
+    //currentOperator = ''
+}
+
+//clears the memory and the current display
+function clearMemory(event) {
+    clearDisplayWindow();
+    calculatingWindow.textContent = '';
+    currentOperator = '';
+    displayValue = '';
 }
 
 //listens for clicks and enters the number into the display window
@@ -48,37 +67,70 @@ console.log(displayWindow.textContent);
 //clears the current display window and stores the current value
 
 function operatorPressed(operator) {
-    console.log('operator pressed');
-    displayValue = displayWindow.textContent;
-    currentOperator = operator.id
-    console.log(`stored value is ${displayValue}`);
-    console.log(`current operator is ${currentOperator}`);
-    clearDisplayWindow();
+    if (displayValue === '') {
+        console.log('operator pressed');
+        displayValue = displayWindow.textContent;
+        currentOperator = operator.id
+        calculatingWindow.textContent = displayValue + operator;
+        clearDisplayWindow();
+    } else {
+        currentTotal = calculatingWindow.textContent + displayWindow.textContent;
+        answer = eval(currentTotal);
+        console.log(currentTotal);
+        console.log(answer);
+        calculatingWindow.textContent = answer + operator;
+        clearDisplayWindow();
+    };
+};
+
+function decimalAdded(event) {
+    console.log('decimal added')
+    let decimalise = displayWindow.textContent + '.';
+    displayWindow.textContent = decimalise;
 }
 
 //division operator
 
 function operateDivide(event) {
     console.log('pressed divide');
-    operatorPressed(divide);
+    operatorPressed('/');
 }
 
 function operateMultiply (event) {
     console.log('pressed multiply');
-    operatorPressed(multiply);
+    operatorPressed('*');
 }
 
+function operateAddition (event) {
+    console.log('pressed add');
+    operatorPressed('+');
+}
+
+function operateSubtract (event) {
+    console.log('pressed subtract');
+    operatorPressed('-');
+}
 
 //calculate
-
 function calculate(event) {
-    if (currentOperator == 'divide') {
-        displayWindow.textContent = (displayValue / displayWindow.textContent);
-        console.log('calculating division');
-        displayValue = '';
-    } else if (currentOperator = 'multiply') {
-        displayWindow.textContent = (displayValue * displayWindow.textContent);
-        console.log('calculating multiplication');
-        displayValue = ''
-    }
-};
+    currentTotal = calculatingWindow.textContent + displayWindow.textContent;
+    answer = eval(currentTotal);
+    console.log(currentTotal);
+    console.log(answer);
+    calculatingWindow.textContent = 'ANSWER BELOW';
+    currentTotal = '';
+    displayValue = '';
+    displayWindow.textContent = answer;
+}
+
+//function calculate(event) {
+    //if (currentOperator == 'divide') {
+        //displayWindow.textContent = (displayValue / displayWindow.textContent);
+        //console.log('calculating division');
+        // = '';
+    //} else if (currentOperator = 'multiply') {
+        //displayWindow.textContent = (displayValue * displayWindow.textContent);
+        //console.log('calculating multiplication');
+        //displayValue = ''
+    //}
+//};
